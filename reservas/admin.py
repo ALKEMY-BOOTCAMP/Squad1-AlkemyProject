9260acc5888cf8fd9b9072eea0d68fce16dfdcca
Gonzalo Arce <gonza_arc@hotmail.com>
2023-05-18 00:29:57 -0300

    Se registro el modelo Servicio en el Admin con atributos

--- reservas/admin.py
+++ reservas/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Servicio
 
-# Register your models here.
+class ServicioAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'precio', 'activo')
+    list_search = ('nombre',)
+    list_filter = ('activo',)
+
+admin.site.register(Servicio, ServicioAdmin)
