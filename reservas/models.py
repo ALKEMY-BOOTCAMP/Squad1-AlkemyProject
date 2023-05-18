58737d9cd8406aacdc98809c118ee6bd4811fc7d
Gonzalo Arce <gonza_arc@hotmail.com>
2023-05-18 00:00:31 -0300

    Se agrego la entidad Servicio

--- reservas/models.py
+++ reservas/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Servicio(models.Model):  
+    nombre = models.CharField(max_length=30)
+    descripcion = models.TextField(blank=True, null = True)
+    precio = models.PositiveIntegerField()
+    activo = models.BooleanField(default=True)
+    
+    def __str__(self):
+        return f"{self.nombre.upper()}, Precio {self.precio}| [ {self.activo} ] "
+
+class Meta:
+    ordering = ['nombre','precio','activo']
