7ccaf4b2afc8ceb87114a0eba51c2b2cb9d7db98
PicoHBK <picohbk322@gmail.com>
2023-05-19 17:12:33 -0300

    ADD formulario para empleado y funcion para la view para que el registro solo sea en metodo post

--- reservas/models.py
+++ reservas/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+##Modelo Empleado
 class Empleado(models.Model):
     apellido = models.CharField(max_length=50)
     nombre = models.CharField(max_length=50)
--- reservas/views.py
+++ reservas/views.py
@@ -1,13 +1,31 @@
 from django.shortcuts import render,get_object_or_404
 from django.shortcuts import render
 from .models import Empleado
+from .empleado_forms import EmpleadoForm
 
 
-#funcion que por busqueda de id activa un empleado y muestra un mensaje por ahora
+#Funcion que por busqueda de id activa un empleado y muestra un mensaje por ahora
 def activar_empleado(request,id):
     empleado = get_object_or_404(Empleado, id = pk)
     empleado.activo = True
     empleado.save()
     
     print("Activado")
+    
+
+#View para registrar empleado que pide que el metodo del request sea POST
+def registrar_empleado(request):
+    if request.method == 'POST':
+        form = EmpleadoForm(request.POST)
+        if form.is_valid():
+            # Procesar los datos del formulario y guardar el nuevo empleado
+            # redireccionar a una página de éxito o realizar cualquier acción necesaria
+            return redirect('exito')
+    else:
+        #Form vacio 
+        form = EmpleadoForm()
+
+    #Implemantar los templates
+    #return render(request, 'registro_empleado.html', {'form': form})
+
     
