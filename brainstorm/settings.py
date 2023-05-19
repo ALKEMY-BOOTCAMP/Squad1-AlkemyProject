9b85e7d9cd33c816efaa73865cde789ac9621758
Oscar Gonzalo Araya <gonxaraya991@gmail.com>
2023-05-18 21:03:19 -0300

    Cliente agregado en admin.py

--- brainstorm/settings.py
+++ brainstorm/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     #Aplicaciones del proyecto
     'reservas',
+    'cliente',
 ]
 
 MIDDLEWARE = [
@@ -125,3 +126,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+ADMINS = [
+    ('admin', 'admin', 'pbkdf2_sha256$600000$8h29quGTsmiYVSaxX1p5Cq$D493EIOjp57JuxePxWe43J4f8w5sP948MQAciYcYwOE='),
+]
