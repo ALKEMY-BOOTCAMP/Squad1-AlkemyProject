1f9333d39c0c8b48aa15192bba53e5e68c05d925
Oscar Gonzalo Araya <gonxaraya991@gmail.com>
2023-05-22 08:03:08 -0300

    formulario de creacion de empleados

--- reservas/urls.py
+++ reservas/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 #from .views import
-from .views import empleados_form_view, empleados_listado
+from .views import empleados_form_view, empleados_listado, empleados_nuevo
 
 app_name = 'reservas'
 
@@ -8,4 +8,5 @@ url_patters = [
    # path()
    path('empleados/modificar/<int:numero_legajo>', empleados_form_view, name='empleados_form_view'),
    path('/empleados/listado', empleados_listado, name='empleados_listado'),
+   path('/empleados/nuevo/', empleados_nuevo, name='empleados_nuevo'),
 ]
--- reservas/views.py
+++ reservas/views.py
@@ -5,6 +5,8 @@ from .models import Empleado
 
 
 # Create your views here.
+
+#Vista del formulario para actualizar el registro de un empleado
 def empleados_form_view(request, pk):
     
     empleado = get_object_or_404(Empleado, id=pk)  # Obtener el objeto Empleado correspondiente al ID proporcionado
@@ -20,8 +22,23 @@ def empleados_form_view(request, pk):
     
     return render(request, 'empleados/empleados_form.html', {'form': form})  # Renderizar el formulario en la plantilla 'empleados/empleados_form.html'
 
+# Vista para listar el registro de empleados
 def empleados_listado(request):
-    empleados = empleados.object.All()
+    empleados = empleados.object.All()  # Obtener todos los objetos de la clase Empleado
     
-    return render(request, 'listar_empleados.html', 
-                {'titulo': 'Lista De Emplelados','empleados': empleados})
+    return render(request, 'listar_empleados.html', {'titulo': 'Lista De Empleados', 'empleados': empleados})
+    # Renderizar la plantilla 'listar_empleados.html', pasando el título y la lista de empleados como contexto
+
+# Vista para crear un nuevo empleado
+def empleados_nuevo(request):
+    
+    if request.method == 'POST':
+        form = EmpleadosForm(request.POST)  # Crear una instancia del formulario EmpleadosForm con los datos enviados en la solicitud POST
+        if form.is_valid():  # Verificar si el formulario es válido
+            form.save()  # Guardar los datos del formulario en la base de datos
+            return redirect('listar_empleados.html')  # Redireccionar a la página de listado de empleados
+    else:
+        form = EmpleadosForm()  # Crear una instancia vacía del formulario EmpleadosForm si la solicitud no es de tipo POST
+    
+    return render(request, 'empleados_nuevo.html', {'form': form})
+    # Renderizar la plantilla 'empleados_nuevo.html', pasando el formulario como contexto
