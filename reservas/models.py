8ed60842c66d3e2c55c00f76046e384d698264ad
Gonzalo Arce <gonza_arc@hotmail.com>
2023-05-24 19:53:10 -0300

    Se agrego el fomulario de dev_Gonzalo_Araya al codigo dev_Gonzalo_Arce

--- reservas/models.py
+++ reservas/models.py
@@ -8,13 +8,12 @@ class Cliente(models.Model):
     apellido = models.CharField(max_length=50)
     activo = models.BooleanField(default=True)
 
-    # Método que devuelve una representación legigle de Cliente en forma de cadena
+    # Método que devuelve una representación legible de Cliente en forma de cadena
     def __str__(self):
-        return f' Cliente: {self.apellido.upper()}, {self.nombre.lower()}, Activo: {self.activo}'
+        return f'Cliente: {self.apellido.upper()}, {self.nombre.lower()}, Activo: {self.activo}'
 
     # Clase que proporciona información útil sobre cómo se almacenan y representan los datos
     # de un cliente, así como cómo se ordenan las instancias de cliente por defecto en consultas de base de datos.
-
     class Meta:
         ordering = ['activo', 'apellido', 'nombre']
 
@@ -28,12 +27,10 @@ class Servicio(models.Model):
     def __str__(self):
         return f"Servicio: {self.nombre.upper()}, Precio: ${self.precio} | [ {self.activo} ] "
 
+    class Meta:
+        ordering = ['nombre', 'precio', 'activo']
 
-class Meta:
-    ordering = ['nombre', 'precio', 'activo']
 
-
-# Modelo Empleado
 class Empleado(models.Model):
     apellido = models.CharField(max_length=50)
     nombre = models.CharField(max_length=50)
@@ -44,23 +41,16 @@ class Empleado(models.Model):
         return f"{self.nombre} {self.apellido}"
 
 
-# Modelo que representa la reserva de un servicio
 class ReservaServicio(models.Model):
-    fecha_creacion = models.DateTimeField(default=datetime.now())
-    fecha_reserva = models.DateTimeField(default=datetime.today())
+    fecha_creacion = models.DateTimeField(default=datetime.now)
+    fecha_reserva = models.DateTimeField(default=datetime.today)
     cliente = models.ForeignKey(
-        Cliente, null=False, blank=False, on_delete=models.CASCADE)
-    # responsable = models.ForeignKey(Coordinador, null=False, blank=False, on_delete=models.CASCADE)
-    # empleado = models.ForeignKey(Empleado, null=False, blank=False, on_delete=models.CASCADE)
-    # servicio = models.ForeignKey(Servicio, null=False, blank=False, on_delete=models.CASCADE)
-    precio = models.PositiveBigIntegerField(null=False, blank=False)
+        Cliente, on_delete=models.CASCADE)
+    precio = models.PositiveIntegerField(null=False, blank=False)
 
-    # Método que devuelve una representación legigle de ReservaDeServicio en forma de cadena
     def __str__(self):
         return f'Reserva De Servicio: {self.fecha_creacion}, {self.cliente}, {self.precio}, {self.fecha_reserva}'
 
-    # Clase que proporciona información útil sobre cómo se almacenan y representan los datos
-    # de una reserva de servicio, así como cómo se ordenan las instancias de la reserva por defecto en consultas de base de datos.
     class Meta:
         ordering = ['fecha_creacion', 'cliente', 'precio']
 
@@ -68,8 +58,8 @@ class ReservaServicio(models.Model):
 class Coordinador(models.Model):
     nombre = models.CharField(max_length=30, null=False, blank=False)
     apellido = models.CharField(max_length=30, null=False, blank=False)
-    numero_documento = models.IntegerField(blank=False, null=False)
-    fecha_alta = models.DateTimeField(default=datetime.today())
+    numero_documento = models.PositiveIntegerField(blank=False, null=False)
+    fecha_alta = models.DateTimeField(default=datetime.today)
     activo = models.BooleanField(default=True)
 
     def __str__(self):
@@ -77,3 +67,4 @@ class Coordinador(models.Model):
 
     class Meta:
         ordering = ['activo', 'apellido', 'numero_documento']
+
